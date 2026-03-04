ead55441895058b02bab49dd276faac6fe52460a
rarfel <faelmvitor@gmail.com>
2026-03-04 18:15:17 -0300

    unstable

--- flake.nix
+++ flake.nix
@@ -2,10 +2,11 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgsUnstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/nixos-unstable";
+      #url = "github:nix-community/home-manager/nixos-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     quickshell = {
