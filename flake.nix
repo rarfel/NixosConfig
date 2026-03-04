982987606b4887e7a55be34792d48388f896a15b
rarfel <faelmvitor@gmail.com>
2026-03-04 20:53:06 -0300

    workaround

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "NixOS configuration";
 
   inputs = {
-    #nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/nixos-unstable";
-      #url = "github:nix-community/home-manager/nixos-25.11";
+      url = "github:nix-community/home-manager/release-25.11";
+      #url = "github:nix-community/home-manager/nixos-unstable";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     quickshell = {
