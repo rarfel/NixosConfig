6d27d11c67ea07c666c1dde3e3f495d7ddc916d7
rarfel <faelmvitor@gmail.com>
2026-03-04 18:33:13 -0300

    unstable both

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "NixOS configuration";
 
   inputs = {
+    #nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgsUnstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/nixos-unstable";
       #url = "github:nix-community/home-manager/nixos-25.11";
