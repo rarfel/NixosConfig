be73859df4ddd565c56fc38d582c3431264c8dbd
rarfel <faelmvitor@gmail.com>
2026-03-10 16:31:21 -0300

    if it works, it works

--- modules/programs/bash.nix
+++ modules/programs/bash.nix
@@ -15,6 +15,9 @@
 
 			ghidra = "_JAVA_AWT_WM_NONREPARENTING=1 ghidra";
 
+			# Crazy workaround to run non-nixos files: alien ./myfile
+			alien = "nix run \"github:thiagokokada/nix-alien#nix-alien\" -- ";
+
 		};
 		initExtra = "fastfetch --kitty-direct /home/rarfel/Images/valknutCheio.png --logo-width 35 --logo-height 15";
 	};
