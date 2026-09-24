6ae0a8ce2033c6cf8a7a589eeaaf63c9ac7b25fb
rarfel <faelmvitor@gmail.com>
2026-09-24 21:22:39 +0000

    Added shotcut to a small terminal

--- modules/programs/nvim/config/lua/vim-config.lua
+++ modules/programs/nvim/config/lua/vim-config.lua
@@ -4,7 +4,16 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 
+-- View errors on line
 vim.keymap.set("n", "<C-o>", vim.diagnostic.open_float, {})
+
+-- Open small terminal
+vim.keymap.set("n", "<C-s>",function ()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0,15)
+end)
 
 vim.opt.number = true
 vim.opt.relativenumber = true
